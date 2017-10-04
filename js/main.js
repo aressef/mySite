7259fa464ec1ee8be5976ebc45a9e2fc5327f177
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-10-03 20:14:56 -0400

    Added methods for each section

--- js/main.js
+++ js/main.js
@@ -1,37 +1,54 @@
 var xhr = new XMLHttpRequest();
 
 // AJAX request for sections html
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-    } else {
-      console.log('Error: ' + xhr.status);
+var ajaxCall = function (section, file) {
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+        sections.section();
+      } else {
+        console.log('Error: ' + xhr.status);
+      }
     }
-  }
-};
+  };
 
-xhr.open('GET', 'projects.html');
-xhr.send(null);
+  xhr.open('GET', file);
+  xhr.send(null);
+};
 
 
 var sections = {
-  about: function() {
+  sectionDiv: document.querySelector('.js-section'),
 
+  about: function(html) {
+    this.sectionDiv.appendChild(html);
+    console.log('about');
   },
 
   projects: function() {
-
+    this.sectionDiv.appendChild(html);
+    console.log('projects');
   },
 
   contact: function() {
-
+    this.sectionDiv.appendChild(html);
+    console.log('contact');
   }
 }
 
 
-document.addEventListener("click", function() {
-  document.querySelector(
-      // figure out how to know which section item was clicked
-  )
-});
+var sectionLinkClicked = document.querySelectorAll('.js-section-link');
+
+for (var i = 0; i < sectionLinkClicked.length; i++) {
+  sectionLinkClicked[i].addEventListener('click', function() {
+
+    if (this.classList.contains('about-me')) {
+      ajaxCall('about', 'about.html');
+    } else if (this.classList.contains('projects')) {
+      ajaxCall('projects', 'projects.html');
+    } else if (this.classList.contains('contact')) {
+      ajaxCall('contact', 'contact.html');
+    }
+  });
+}
