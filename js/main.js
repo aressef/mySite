92b8b012b617e65037b3aa899d3b134da3d265df
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-10-04 20:45:21 -0400

    Sections can now be inserted into DOM via AJAX

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 var xhr = new XMLHttpRequest();
+var sectionDiv = document.querySelector('.js-section');
 
 // AJAX request for sections html
 var ajaxCall = function (section, file) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        console.log(xhr.responseText);
-        sections.section();
+        // console.log(xhr.responseText);
+        var sectionDivHTML = document.createRange().createContextualFragment(xhr.responseText);
+        // sectionDivHTML = xhr.responseText;
+        // console.log(sectionDivHTML);
+        section(sectionDivHTML);
       } else {
         console.log('Error: ' + xhr.status);
       }
     }
   };
+
+
+
 
   xhr.open('GET', file);
   xhr.send(null);
@@ -19,36 +26,41 @@ var ajaxCall = function (section, file) {
 
 
 var sections = {
-  sectionDiv: document.querySelector('.js-section'),
-
-  about: function(html) {
-    this.sectionDiv.appendChild(html);
-    console.log('about');
+  about: function(sectionHTML) {
+    while (sectionDiv.hasChildNodes()) {
+      sectionDiv.removeChild(sectionDiv.lastChild);
+    }
+    sectionDiv.appendChild(sectionHTML);
   },
 
-  projects: function() {
-    this.sectionDiv.appendChild(html);
-    console.log('projects');
+  projects: function(sectionHTML) {
+    while (sectionDiv.hasChildNodes()) {
+      sectionDiv.removeChild(sectionDiv.lastChild);
+    }
+    sectionDiv.appendChild(sectionHTML);
   },
 
-  contact: function() {
-    this.sectionDiv.appendChild(html);
-    console.log('contact');
+  contact: function(sectionHTML) {
+    while (sectionDiv.hasChildNodes()) {
+      sectionDiv.removeChild(sectionDiv.lastChild);
+    }
+    sectionDiv.appendChild(sectionHTML);
   }
-}
+};
 
 
-var sectionLinkClicked = document.querySelectorAll('.js-section-link');
+var sectionLinkClicked = document.querySelectorAll('.js-sections-link');
 
 for (var i = 0; i < sectionLinkClicked.length; i++) {
   sectionLinkClicked[i].addEventListener('click', function() {
 
     if (this.classList.contains('about-me')) {
-      ajaxCall('about', 'about.html');
+      ajaxCall(sections.about, 'about.html');
     } else if (this.classList.contains('projects')) {
-      ajaxCall('projects', 'projects.html');
+      ajaxCall(sections.projects, 'projects.html');
     } else if (this.classList.contains('contact')) {
-      ajaxCall('contact', 'contact.html');
+      ajaxCall(sections.contact, 'contact.html');
     }
+
   });
 }
