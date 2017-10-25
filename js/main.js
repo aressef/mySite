7c779d49399d19f1ddc4751d67da964c771904c1
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-10-25 17:13:22 -0400

    Refactored JS to clean it up

--- js/main.js
+++ js/main.js
@@ -2,27 +2,8 @@ var xhr = new XMLHttpRequest();
 var sectionDiv = document.querySelector('.js-section');
 var isProjectsSelected;
 
-// AJAX request for sections html
-var ajaxCall = function (section, file) {
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var sectionDivHTML = document.createRange().createContextualFragment(xhr.responseText);
-        section(sectionDivHTML);
-        isProjectsSelected = sectionDivHTML.firstElementChild;
-      } else {
-        console.log('Error: ' + xhr.status);
-      }
-    }
-  };
-
-  xhr.open('GET', file);
-
-  xhr.send(null);
-};
-
-
-var sections = {
+// Removes section currently displayed and replaces with section clicked
+var displaySection = {
   about: function(sectionHTML) {
     while (sectionDiv.hasChildNodes()) {
       sectionDiv.removeChild(sectionDiv.lastChild);
@@ -35,7 +16,7 @@ var sections = {
       sectionDiv.removeChild(sectionDiv.lastChild);
     }
     sectionDiv.appendChild(sectionHTML);
-    playVideoOnHover();
+    style.playVideoOnHover();
   },
 
   contact: function(sectionHTML) {
@@ -49,49 +30,96 @@ var sections = {
 
 var sectionLinkClicked = document.querySelectorAll('.js-sections-link');
 
-for (var i = 0; i < sectionLinkClicked.length; i++) {
-  sectionLinkClicked[i].addEventListener('click', function() {
+var ajax = {
+  // AJAX request for sections html
+  ajaxCall: function (section, file) {
+    xhr.onreadystatechange = function() {
 
-    if (this.classList.contains('about-link')) {
-      ajaxCall(sections.about, 'html/about.html');
-    } else if (this.classList.contains('projects-link')) {
-      if (window.innerWidth > 991) {
-        ajaxCall(sections.projects, 'html/projects-large.html')
-      } else {
-        ajaxCall(sections.projects, 'html/projects-small-and-med.html');
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var sectionDivHTML = document.createRange().createContextualFragment(xhr.responseText);
+          section(sectionDivHTML);
+          isProjectsSelected = sectionDivHTML.firstElementChild;
+        } else {
+          console.log('Error: ' + xhr.status);
+        }
       }
-    } else if (this.classList.contains('contact-link')) {
-      ajaxCall(sections.contact, 'html/contact.html');
+
+    };
+    xhr.open('GET', file);
+    xhr.send(null);
+  },
+  // Calls for an AJAX request of the section link that is clicked
+  requestSectionOnClick: function () {
+    for (var i = 0; i < sectionLinkClicked.length; i++) {
+      sectionLinkClicked[i].addEventListener('click', function() {
+    
+        if (this.classList.contains('about-link')) {
+          ajax.ajaxCall(displaySection.about, 'html/about.html');
+        } else if (this.classList.contains('projects-link')) {
+          if (window.innerWidth > 991) {
+            ajax.ajaxCall(displaySection.projects, 'html/projects-large.html')
+          } else {
+            ajax.ajaxCall(displaySection.projects, 'html/projects-small-and-med.html');
+          }
+        } else if (this.classList.contains('contact-link')) {
+          ajax.ajaxCall(displaySection.contact, 'html/contact.html');
+        }
+    
+      });
     }
-
-  });
-}
-
-var loadProjectPageByScreenSize = function() {
-  if (isProjectsSelected != undefined) {
-    if (window.innerWidth > 991 && isProjectsSelected.classList.contains('projects')) {
-      ajaxCall(sections.projects, 'html/projects-large.html')
-    } else {
-      ajaxCall(sections.projects, 'html/projects-small-and-med.html');
+  },
+  // Returns correct projects.html file when screen is manually resized
+  loadProjectPageByScreenSize: function() {
+    if (isProjectsSelected != undefined) {
+      if (window.innerWidth > 991 && isProjectsSelected.classList.contains('projects')) {
+        ajax.ajaxCall(displaySection.projects, 'html/projects-large.html')
+      } else {
+        ajax.ajaxCall(displaySection.projects, 'html/projects-small-and-med.html');
+      }
     }
   }
 }
 
-window.addEventListener('resize', loadProjectPageByScreenSize, false);
 
-
-function playVideoOnHover() {
-
-  var projectVideos = document.querySelectorAll('.project-vids');
-
-  for (var i = 0; i < projectVideos.length; i++) {
-
-    projectVideos[i].addEventListener('mouseenter', function() {
-      this.play();
-    });
-
-    projectVideos[i].addEventListener('mouseout', function() {
-      this.pause();
-    });
+var style = {
+  // Plays project video when user hovers / pauses when user stops
+  playVideoOnHover: function() { 
+    var projectVideos = document.querySelectorAll('.project-vids');
+  
+    for (var i = 0; i < projectVideos.length; i++) {
+      projectVideos[i].addEventListener('mouseenter', function() {
+        this.play();
+      });
+      projectVideos[i].addEventListener('mouseout', function() {
+        this.pause();
+      });
+    }
+  },
+  // Section that is selected is made bold / others will be faded
+  highLightSection: function() {
+    for (var i = 0; i < sectionLinkClicked.length; i++) {
+      sectionLinkClicked[i].addEventListener('click', function() {
+    
+        if (this.classList.contains('about-link')) {
+          this.classList.add('');
+        } else if (this.classList.contains('projects-link')) {
+          if (window.innerWidth > 991) {
+            this.classList.add('');
+          } else {
+            this.classList.add('');
+          }
+        } else if (this.classList.contains('contact-link')) {
+          this.classList.add('');
+        }
+    
+      });
+    } 
   }
 }
+
+
+// window.addEventListener('click', function, false);
+window.addEventListener('resize', ajax.loadProjectPageByScreenSize, false);
+
+ajax.requestSectionOnClick();
